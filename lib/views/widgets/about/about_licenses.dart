098783e1f9e945bf6b9aa9e673744bfd4c034f1e
AniLoop <mtlbkuchbhi47@gmail.com>
2026-09-14 04:22:52 +0530

    Remove incompatible icons_plus dependency

--- lib/views/widgets/about/about_licenses.dart
+++ lib/views/widgets/about/about_licenses.dart
@@ -1,7 +1,7 @@
 import 'package:aniloop/controllers/runtime_data_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:aniloop/themes/themes.dart';
-import 'package:icons_plus/icons_plus.dart';
+ 
 import 'package:url_launcher/url_launcher.dart';
 
 class PackagesLicenses extends StatelessWidget {
@@ -70,7 +70,7 @@ class PackagesLicenses extends StatelessWidget {
                 },
                 // ignore: prefer_const_constructors
                 child: Icon(
-                  Bootstrap.github,
+                  Icons.code,
                   size: 45,
                   color: !RuntimeController.isDarkMode
                       ? Themes.dark.primaryColor
@@ -308,10 +308,8 @@ class PackagesLicenses extends StatelessWidget {
                       InkWell(
                         onTapUp: (details) {
                           launchUrl(Uri.parse(
-                              "https://raw.githubusercontent.com/chouhan-rahul/icons_plus/main/LICENSE"));
                         },
                         child: const Text(
-                          "icons_plus,",
                           style: TextStyle(
                             color: Color.fromARGB(255, 0, 140, 255),
                             decoration: TextDecoration.underline,
